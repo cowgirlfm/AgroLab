a8da06a62374025d352d6fd1b291c28cf804ac1f
cowgirlfm <tcandream@gmail.com>
2025-05-28 21:40:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const video = document.getElementById('introVideo');
+const source = video.querySelector('source');
 const container = document.getElementById('introContainer');
+
+
+const esCelular = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+
+if (esCelular) {
+  source.setAttribute('src', '/animacion-celular.mp4');
+} else {
+  source.setAttribute('src', '/animacion.mp4');
+}
+
+
+video.load();
 
 video.addEventListener('ended', () => {
   container.classList.add('fade-out');
