f6265a3a3e20012afaac598bb5cab67a1fe280ea
cowgirlfm <tcandream@gmail.com>
2025-05-28 22:09:45 -0500

    Update livinglab.js

--- livinglab.js
+++ livinglab.js
@@ -17,35 +17,36 @@ const farmbot = document.getElementById('farmbot');
 let lechugaMostrada = false;
 
 farmbot.addEventListener('click', function () {
-
   if (farmbot.classList.contains('animar')) return;
 
+  const isMobile = window.innerWidth <= 768;
   farmbot.classList.add('animar');
+  farmbot.style.animationName = isMobile ? 'moverAbajo' : 'moverDerecha';
 
   setTimeout(() => {
     farmbot.classList.remove('animar');
+    farmbot.style.animationName = ''; // limpiar animación
+
     if (!lechugaMostrada) {
-  const macetaIds = ['maceta1', 'maceta2', 'maceta3', 'maceta4'];
+      const macetaIds = ['maceta1', 'maceta2', 'maceta3', 'maceta4'];
 
-  macetaIds.forEach((id) => {
-    const maceta = document.getElementById(id);
-    if (maceta) {
-      const lechuga = document.createElement('img');
-      lechuga.src = 'grupo-lechuga.png';
-      lechuga.alt = 'Grupo de lechugas';
-      lechuga.style.position = 'absolute';
-      lechuga.style.width = '170px';
-      lechuga.style.top = '7px';
-      lechuga.style.left = '50%';
-      lechuga.style.transform = 'translateX(-50%)';
-      lechuga.style.pointerEvents = 'none';
+      macetaIds.forEach((id) => {
+        const maceta = document.getElementById(id);
+        if (maceta) {
+          const lechuga = document.createElement('img');
+          lechuga.src = 'grupo-lechuga.png';
+          lechuga.alt = 'Grupo de lechugas';
+          lechuga.style.position = 'absolute';
+          lechuga.style.width = '170px';
+          lechuga.style.top = '7px';
+          lechuga.style.left = '50%';
+          lechuga.style.transform = 'translateX(-50%)';
+          lechuga.style.pointerEvents = 'none';
 
-      maceta.appendChild(lechuga);
+          maceta.appendChild(lechuga);
+        }
+      });
+      lechugaMostrada = true;
     }
-  });
-  lechugaMostrada = true;
-}
-}, 4000);
+  }, 4000);
 });
-
-
